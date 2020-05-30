758bfc7368e2fe62c58a5dbc374a0ac1c35cbe79
Cesar <polaert.cesar@hotmail.com>
2020-05-30 02:45:29 +0200

    mémoire cache + Constante

---
+++ app/src/main/java/com/example/deezerapirecyclerview/Constante.java
@@ -0,0 +1,5 @@
+package com.example.deezerapirecyclerview;
+
+public class Constante {
+    static String KEY_DEEZER_LIST = "jsonListPlaylist";
+}
--- app/src/main/java/com/example/deezerapirecyclerview/MainActivity.java
+++ app/src/main/java/com/example/deezerapirecyclerview/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.deezerapirecyclerview;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -9,7 +11,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ListAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    private SharedPreferences sharedPreferences;
+    private Gson gson;
 
     private static final String BASE_URL = "https://api.deezer.com/";
 
@@ -32,6 +38,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sharedPreferences = getSharedPreferences("AppDeezer", Context.MODE_PRIVATE);
+        gson = new GsonBuilder()
+                .setLenient()
+                .create();
 
         makeAPICall();
     }
@@ -48,10 +59,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void makeAPICall(){
-        Gson gson = new GsonBuilder()
-                .setLenient()
-                .create();
-
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create(gson))
@@ -65,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<ListPlaylist> call, Response<ListPlaylist> response) {
                 if(response.isSuccessful() && response.body()!=null){
                     List<Playlist> listPlaylist= response.body().getData();
+                    saveList(listPlaylist);
                     showList(listPlaylist);
                 }
                 else {
@@ -79,7 +87,29 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void saveList(List<Playlist> listPlaylist) {
+        String jsonString = gson.toJson(listPlaylist);
+        sharedPreferences
+                .edit()
+                .putString(Constante.KEY_DEEZER_LIST,jsonString)
+                .apply();
+        Toast.makeText(getApplicationContext(),"Saved list",Toast.LENGTH_LONG).show();
+
+    }
+
     private void showError() {
-        Toast.makeText(getApplicationContext(),"API error",Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(),"download from cache",Toast.LENGTH_LONG).show();
+        List<Playlist> listPlaylist = getDataFromCache();
+        if (listPlaylist != null){
+            showList(listPlaylist);
+        } else Toast.makeText(getApplicationContext(),"no cache to download, unable to reach API online",Toast.LENGTH_LONG).show();
+    }
+
+    private List<Playlist> getDataFromCache() {
+        String listPlaylist = sharedPreferences.getString(Constante.KEY_DEEZER_LIST,null);
+        if (listPlaylist==null) return null;
+        Type listType = new TypeToken<List<Playlist>>(){}.getType();
+        return gson.fromJson(listPlaylist, listType);
+
     }
 }
